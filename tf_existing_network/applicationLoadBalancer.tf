4ba344925596d23b95666c74a369e549cd79cf09
Michael Smith <smithatlanta@gmail.com>
2020-03-26 18:19:14 -0400

    fix for alb

--- tf_existing_network/applicationLoadBalancer.tf
+++ tf_existing_network/applicationLoadBalancer.tf
@@ -58,6 +58,19 @@ resource "aws_lb_listener" "looker_lb_listener" {
   }
 }
 
+resource "aws_lb_listener" "looker_api_lb_listener" {
+  load_balancer_arn = aws_lb.looker_lb.arn
+  port              = var.api_listener_port
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.looker_api_lb_tg.arn
+  }
+}
+
 resource "aws_lb_target_group" "looker_api_lb_tg" {
   name     = "${var.prefix}-api-lb-tg"
   port     = var.api_listener_port
@@ -80,38 +93,3 @@ resource "aws_lb_target_group" "looker_api_lb_tg" {
   }
 }
 
-resource "aws_lb" "looker_api_lb" {
-  name               = "${var.prefix}-api-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.looker_lb.id}"]
-  subnets            = ["${var.public_subnet1_id}", "${var.public_subnet2_id}"]
-
-  access_logs {
-    bucket  = aws_s3_bucket.s3_looker_access_log_bucket.id
-    prefix  = "looker-api-lb"
-    enabled = true
-  }
-
-  tags = {
-    Name          = "${var.prefix}_api_alb"
-    application   = var.tag_application
-    contact-email = var.tag_contact_email
-    customer      = var.tag_customer
-    team          = var.tag_team
-    environment   = var.tag_environment
-  }
-}
-
-resource "aws_lb_listener" "looker_api_lb_listener" {
-  load_balancer_arn = aws_lb.looker_api_lb.arn
-  port              = var.api_listener_port
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.looker_api_lb_tg.arn
-  }
-}
